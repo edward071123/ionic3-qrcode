25cda4a1def3f65a9f1dbd6ea60220b2fe6ca542
edward <leonardo071123@gmail.com>
2018-01-24 13:41:48 +0800

    install qrscanner package to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,33 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { QRScanner } from '@ionic-native/qr-scanner';
 
-import { MyApp } from './app.component';
+import { QRScannerApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { QrscannerPage } from '../pages/qrscanner/qrscanner';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    QRScannerApp,
+    HomePage,
+    QrscannerPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(QRScannerApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    QRScannerApp,
+    HomePage,
+    QrscannerPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    QRScanner
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+import { QrscannerPage } from '../qrscanner/qrscanner';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public qrScanner: QRScanner) {
 
+  }
+  scanner() {
+    this.navCtrl.push(QrscannerPage);
   }
 
 }
